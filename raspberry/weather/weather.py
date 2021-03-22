03070eccfa1d9eea4e1441ac7360f420f4694f6a
Shaurya <sonu.hooda@gmail.com>
2021-03-22 20:03:08 +0530

    weather

--- raspberry/weather/weather.py
+++ raspberry/weather/weather.py
@@ -5,9 +5,10 @@ import json
 import re
 
 weatherFile = '/home/pi/pythonwork/keypad/weather/weather.html';
+weatherFileHourly = '/home/pi/pythonwork/keypad/weather/weatherHourly.html';
 
 class weatherObj:  
-    def __init__(self, date, forecast, percent):
+    def __init__(self, date, forecast, percent, wind):
         date = date.replace("Sun", "Sunday, ")
         date = date.replace("Mon", "Monday, ")
         date = date.replace("Tue", "Tuesday, ")
@@ -19,20 +20,41 @@ class weatherObj:
         
         forecast = forecast.replace("T-Storms", "Thunderstorm, ")
         self.forecast = forecast
-        self.percent = percent
+        self.percent = percent.replace("%", "")
+        self.wind = wind.replace("km/h", "");
     def __str__(self):
-        return self.date +" , " +self.forecast + " , "+ self.percent +" ,"
+        return self.date +" , " +self.forecast + " , "+ self.percent +" ," +self.wind
         
 
 def downloadWeather():
     r = requests.get("https://weather.com/en-IN/weather/tenday/l/6a6f9bf8a5d6b2953fd3a5e17b0d4bc9d4eb2f4265f03368500f754be20d0958", allow_redirects=True) 
     open(weatherFile, 'wb').write(r.content)
     
+def downloadWeatherHourly():
+    r = requests.get("https://weather.com/en-IN/weather/hourbyhour/l/6a6f9bf8a5d6b2953fd3a5e17b0d4bc9d4eb2f4265f03368500f754be20d0958", allow_redirects=True) 
+    open(weatherFileHourly, 'wb').write(r.content)
+    
+def extractWeatherFieldsDay():
+    downloadWeather();
+    return extractWeatherFields(weatherFile);
 
-def extractWeatherFields():
+def extractWeatherFieldsHour():
+    downloadWeatherHourly();
+    weatherForecast = extractWeatherFields(weatherFileHourly);
+    rainForecast =[];
+    i = 0;
+    for obj in weatherForecast:
+        i += 1;
+        if ( i < 14 and ( int(obj.percent)  > 40 or int(obj.wind)  > 10) ) :
+            rainForecast.append(obj)
+            print(obj)
+    return rainForecast;
+
+
+def extractWeatherFields(fileName):
     downloadWeather();
     weatherForecast = []
-    f = open(weatherFile, "r")
+    f = open(fileName, "r")
     weatherData = f.read()
     
     start = 0;
@@ -61,13 +83,22 @@ def extractWeatherFields():
         start = weatherData.index(percentString, start) + len(percentString)
         end = weatherData.index("</span>",start)
         percent = weatherData[start:end]
-        weather_Obj = weatherObj(date, forecast, percent)
+        
+        #find wind speed
+        windSpeedString = "<span data-testid=\"Wind\" class=\"Wind--windWrapper--1Va1P undefined\">"
+        start = weatherData.index(windSpeedString, start) + len(windSpeedString)
+        windSpeedString = "<!-- -->"
+        start = weatherData.index(windSpeedString, start) + len(windSpeedString)
+        end = weatherData.index("</span>",start)
+        wind = weatherData[start:end]
+        
+        weather_Obj = weatherObj(date, forecast, percent, wind)
         weatherForecast.append( weather_Obj )
         #print(weather_Obj)
     return weatherForecast;
 
 def findRain():
-    weatherForecast = extractWeatherFields();
+    weatherForecast = extractWeatherFieldsDay();
     rainForecast =[];
     i = 0;
     for obj in weatherForecast:
@@ -78,5 +109,6 @@ def findRain():
     return rainForecast;
     
 
-
+#findRain();
+extractWeatherFieldsHour();
 
